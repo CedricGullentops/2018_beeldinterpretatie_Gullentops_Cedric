de21bcc5b28a085a39bd9357949368cc4817e5e3
CedricGullentops <cedricgullentops@hotmail.com>
2018-10-24 17:34:44 +0200

    Update

--- sessie_0/main.cpp
+++ sessie_0/main.cpp
@@ -6,6 +6,7 @@ using namespace cv;
 
 int main(int argc, const char** argv)
 {
+    //Parser voor events te tonen zoals errors.
     CommandLineParser parser(argc, argv,
     "{ help h usage ? |  | show this message }"
     "{ image_grey ig  |  | (required) path to image }"
@@ -23,10 +24,11 @@ int main(int argc, const char** argv)
         return -1;
     }
 
+    //Images inlezen en opslagen in Mat variabelen.
     string image_gray_location(parser.get<string>("image_grey"));
     string image_colour_location(parser.get<string>("image_colour"));
     if (image_gray_location.empty() || image_colour_location.empty()){
-        cerr << "Something wrong in the given arguments";
+        cerr << "Something wrong in the given arguments" << endl;
         parser.printMessage();
         return -1;
     }
@@ -40,25 +42,30 @@ int main(int argc, const char** argv)
         return -1;
     }
 
+    //Toon de twee images.
     namedWindow("EAvise Logo - grayscale", WINDOW_AUTOSIZE);
     namedWindow("EAvise Logo - colour", WINDOW_AUTOSIZE);
     imshow("EAvise Logo - grayscale", g_image);
     imshow("EAvise Logo - colour", c_image);
     waitKey(0);
 
-    vector<Mat> c_channels;
+    //Splits de kleur foto op in 3 kanalen en laat elks van deze zien.
+    vector<Mat> channels;
     split(c_image,channels);
 
     namedWindow("Blue", WINDOW_AUTOSIZE);
-    namedWindow("Green", WINDOW_AUTOSIZE);
-    namedWindow("Red", WINDOW_AUTOSIZE);
     imshow("Blue", channels[0]);
+    namedWindow("Green", WINDOW_AUTOSIZE);
     imshow("Green", channels[1]);
+    namedWindow("Red", WINDOW_AUTOSIZE);
     imshow("Red", channels[2]);
 
+    waitKey(0);
+
+    //Converteer de kleurenfoto naar een zwart-wit foto en toon deze en print elke pixelwaarde af.
     Mat converted;
     cvtColor(c_image, converted, COLOR_BGR2GRAY);
-    namedWindow("Converted picture", WINDOW_AUTOSIZE)
+    namedWindow("Converted picture", WINDOW_AUTOSIZE);
     imshow("Converted picture", converted);
 
     for (int i=0; i<converted.rows; i++){
@@ -70,11 +77,14 @@ int main(int argc, const char** argv)
     }
     cout << endl;
 
+    waitKey(0);
+
+    //Laatste foto met enkele willekeurige figuren.
     Mat canvas = Mat::zeros(Size(250,250), CV_8UC3);
-    rectangle(canvas, Rect(150,150), Scalar(255,255,0), 2);
-    circle(canvas, Point(150,150), 75, Scalar(255,255,0), 2);
+    rectangle(canvas, Point(50,50), Point(150,150), Scalar(255,255,100), 2);
+    circle(canvas, Point(150,150), 75, Scalar(50,50,50), 2);
     line(canvas, Point(0, 0), Point(150, 150), Scalar(100, 100, 100), 5);
-    namedWindow("canvas", WINDOW_AUTOSIZE)
+    namedWindow("canvas", WINDOW_AUTOSIZE);
     imshow("canvas", canvas);
 
     waitKey(0);
